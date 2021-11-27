2af7e30ae0ab51313d2630604c96396b9493f814
ruslanyar <styxrus@yandex.ru>
2021-11-27 23:39:28 +0300

    Added sticky onscroll hide/show header

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,24 @@
-const menuBtn = document.querySelector('.header__menu-button');
-const menu = document.querySelector('.header__menu');
-const closeBtn = document.querySelector('.header__close-btn');
+const header = document.querySelector('.header');
+const menuBtn = header.querySelector('.header__menu-button');
+const menu = header.querySelector('.header__menu');
+const closeBtn = header.querySelector('.header__close-btn');
 
 menuBtn.addEventListener('click', () => menu.classList.add('header__menu_is-active'));
 closeBtn.addEventListener('click', () => menu.classList.remove('header__menu_is-active'));
+
+let prevScroll = window.scrollY;
+let currentScroll;
+
+window.addEventListener('scroll', () => {
+  currentScroll = window.scrollY;
+
+  const headerHidden = header.classList.contains('header_hidden');
+
+  if (currentScroll > prevScroll && !headerHidden) {
+    header.classList.add('header_hidden');
+  } else if (currentScroll < prevScroll && headerHidden) {
+    header.classList.remove('header_hidden');
+  }
+
+  prevScroll = currentScroll;
+});
